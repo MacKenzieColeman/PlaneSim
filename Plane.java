3de30bc5d95280a6c4eceb949b0b31c4495e4bf2
bkopfdev <bkopf8@gmail.com>
2024-10-26 14:40:55 -0400

    organization
    Commented what we need, started classes

--- Plane.java
+++ Plane.java
@@ -1,24 +1,40 @@
 import java.text.NumberFormat;
 
+/*
+ * Methods: Initialization, takeoff, landing.
+ * 
+ * Variables: Flight number, plane status of either arriving or departing
+ * 
+ * 
+ */
+
 public class Plane {
 
-	public int WaitTime;
-    private boolean Waiting;
+	public int airWaitTime, groundWaitTime;
+    public int flightNumber;
+    public Runway currentRunway;
+    public boolean arriving; //arriving is true if arriving (going to or on land), and false if departing (leaving or on land).
 
 	
-	public Plane()
+	public Plane(Runway runway, int number)
 	{
-		
+        currentRunway = runway;
+		flightNumber = number;
+        arriving = true; //when a plane is created, it must be arriving
 	}
 
-    public Update(){
-        if(Waiting == true) WaitTime++;
+    public void Update(){
+        if(arriving == true) airWaitTime++;
+        else groundWaitTime++;
     }
 
-    public TakeOff(){
+    public void Land(){
+        arriving = false; //now on land
+        currentRunway.newLanding(Plane.this);
 
     }
 
-    public Land(){
-        Waiting = true;
+    public void TakeOff(){
+        currentRunway.newTakeoff();
     }
+}
--- Runway.java
+++ Runway.java
@@ -1,7 +1,30 @@
 import java.util.Queue;
 import java.util.LinkedList;
 
+/*
+ * Should prioriize landing a plane then a new one taking off (Better to wait on ground then in air)
+ * SO: A plane can only take off if landing queue is empty
+ * 
+ * Keep statistics on: Number of plane sprocessed, 
+ * average time spent waiting, number of planes (if any) refused service(???).
+ * 
+ */
+
 public class Runway {
-    Queue<Integer> InAir = new LinkedList<>();
-    Queue<Integer> OnGround = new LinkedList<>();
+    public int maxQueueSize;
+    Queue<Plane> Landing = new LinkedList<>(); //planes waiting to land
+    Queue<Plane> Takeoff = new LinkedList<>(); //planes waiting to takeoff
+
+    public Runway(int size){
+        maxQueueSize = size;
+    }
+
+    public void newLanding(Plane plane){
+        Landing.add(plane); //adds new plane to those waiting to land!
+    }
+
+    public void newTakeoff(){
+        if(Takeoff.size() < maxQueueSize) Takeoff.add(Landing.remove()); //should remove the plane from landing, and immediately add it to takeoff
+        else System.out.println("Full Airport!");
+    }
 }
