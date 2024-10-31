81e1d8f5b33097af708643c65a5dd9f56bfe96a6
bkopfdev <bkopf8@gmail.com>
2024-10-31 11:59:26 -0400

    Comments and Plane class

--- Plane.java
+++ Plane.java
@@ -9,34 +9,32 @@ import java.text.NumberFormat;
 
 public class Plane {
 
-	public int airWaitTime, groundWaitTime;
-    public int flightNumber;
-    public Runway currentRunway;
+	public int airWaitTime, groundWaitTime; //Wait time in air or on ground, in Time Units
+    public int flightNumber; //Flight number of current object
+    public Runway currentRunway; //Current runway
     public boolean arriving; //arriving is true if arriving (going to land), and false if departing (leaving or on land).
 
 
 	public Plane(Runway runway, int number)
 	{
-        currentRunway = runway;
-		flightNumber = number;
-        arriving = true; //when a plane is created, it must be arriving
+        currentRunway = runway; //Sets plane's runway
+		flightNumber = number; //Sets plane's number
+        arriving = true; //when a plane is created, it is arriving at the airport
 	}
 
-    public void Update(){
-        if(arriving == true) airWaitTime++;
-        else groundWaitTime++;
+    public void Update(){ //Updates every time interval
+        if(arriving == true) airWaitTime++; //If the plane is in the air, increase airwaittime
+        else groundWaitTime++; //If plane is on the ground, increase ground wait time
     }
 
-    public int getNumber() {
+    public int getNumber() { //Getter for the flight number
       return flightNumber;
     }
 
-    public void Land(){
-        arriving = false; //now on land
-        currentRunway.newLanding(Plane.this);
-    }
-
-    public void TakeOff(){
-        currentRunway.newTakeoff();
-    }
+    /*
+     * The project calls for a Takeoff() and Land() method, however, we decided it was unnecessary for this specific class.
+     * When juggling the pros and cons of it, we realized it would be far less complicated if we handled all the taking off
+     * and landing in the Runway class. This is because the queues that handle both taking off and landing are both already
+     * in that class, so it wouldnt make sense to unnecessarily reach out to another class when it can all be handled together.
+     */
 }
